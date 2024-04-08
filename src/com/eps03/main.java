2e5d09dbe13b8e8babaccfa574ec44f4b9664132
essram <esarama22@gmail.com>
2024-04-08 20:46:51 +0700

    masih pusing euy,lanjut besok

--- src/com/eps03/main.java
+++ src/com/eps03/main.java
@@ -10,12 +10,14 @@ class Mahasiswa{
     Mahasiswa(String inputNama, String inputNIM){
         nama = inputNama;
         NIM = inputNIM;
-        System.out.println(nama);
-        System.out.println(NIM);
+        
     }
 }
 public class main {
     public static void main(String[] args) {
         Mahasiswa mahasiswa1 = new Mahasiswa("asep", "101");
+        System.out.println(mahasiswa1.nama);
     }
+
+
 }
